5b7c26d1b2d6ad5fea7d0e2a46a5f9aeea0bdd45
disrupted <hi@salomonpopp.me>
2022-07-14 20:16:37 +0200

    build: update parcel_css to alpha 29

--- src/format_text.rs
+++ src/format_text.rs
@@ -5,12 +5,6 @@ use dprint_core::configuration::resolve_new_line_kind;
 use parcel_css::stylesheet::{ParserOptions, PrinterOptions, StyleSheet};
 use std::path::Path;
 
-const PARSER_OPTS: ParserOptions = ParserOptions {
-    nesting: true,
-    custom_media: false,
-    css_modules: false,
-    source_index: 0,
-};
 const PRINTER_OPTS: PrinterOptions = PrinterOptions {
     minify: false,
     source_map: None,
@@ -20,8 +14,16 @@ const PRINTER_OPTS: PrinterOptions = PrinterOptions {
 };
 
 pub fn format_text(_file_path: &Path, text: &str, config: &Configuration) -> Result<String> {
-    let filename = "";
-    let stylesheet = match StyleSheet::parse(filename, text, PARSER_OPTS) {
+    let parser_opts = ParserOptions {
+        nesting: true,
+        custom_media: false,
+        css_modules: None,
+        source_index: 0,
+        filename: String::new(),
+        error_recovery: false,
+        warnings: None,
+    };
+    let stylesheet = match StyleSheet::parse(text, parser_opts) {
         Ok(v) => v,
         Err(_) => {
             eprintln!("Error parsing file");

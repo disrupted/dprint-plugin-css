60965a551fbc2caaf42ea21e5871864bbc133287
Salomon Popp <hi@salomonpopp.me>
2022-07-14 22:05:06 +0200

    build: update parcel_css to alpha 29 (#4)

--- src/format_text.rs
+++ src/format_text.rs
@@ -5,12 +5,6 @@ use dprint_core::configuration::resolve_new_line_kind;
 use parcel_css::stylesheet::{ParserOptions, PrinterOptions, StyleSheet};
 use std::path::Path;
 
-const PARSER_OPTS: ParserOptions = ParserOptions {
-    nesting: true,
-    custom_media: false,
-    css_modules: false,
-    source_index: 0,
-};
 const PRINTER_OPTS: PrinterOptions = PrinterOptions {
     minify: false,
     source_map: None,
@@ -20,9 +14,16 @@ const PRINTER_OPTS: PrinterOptions = PrinterOptions {
 };
 
 pub fn format_text(_file_path: &Path, text: &str, config: &Configuration) -> Result<String> {
-    let filename = "".to_string();
-    // TODO: get rid of unwrap
-    let stylesheet = match StyleSheet::parse(filename, text, PARSER_OPTS) {
+    let parser_opts = ParserOptions {
+        nesting: true,
+        custom_media: false,
+        css_modules: None,
+        source_index: 0,
+        filename: String::new(),
+        error_recovery: false,
+        warnings: None,
+    };
+    let stylesheet = match StyleSheet::parse(text, parser_opts) {
         Ok(v) => v,
         Err(_) => {
             eprintln!("Error parsing file");
@@ -47,7 +48,7 @@ pub fn format_text(_file_path: &Path, text: &str, config: &Configuration) -> Res
             text.replace("\r\n", "\n")
         } else {
             // lazy
-            text.replace("\r\n", "\n").replace("\n", "\r\n")
+            text.replace("\r\n", "\n").replace('\n', "\r\n")
         },
     )
 }
